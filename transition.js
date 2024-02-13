52c898f4ceceef97aeee8d4c75dd7d1306a4b5ae
AnguloRecto <120491110+Angulorecto@users.noreply.github.com>
2024-02-13 14:19:30 -0500

    Update transition.js

--- transition.js
+++ transition.js
@@ -8,7 +8,7 @@ function start() {
   document.getElementById("box2").style.animation = "up 1s";
   document.getElementById("box1").style.display = "block";
   document.getElementById("box2").style.display = "block";
-  document.getElementById("hammer").style.animation = "fade 1s";
+  document.getElementById("hammer").style.animation = "fade 2s";
   document.getElementById("hammer").style.display = "block";
   // Note: this line's timing has to be 0.15 less then the 3 above.
   setTimeout(blueDone, 0850)
