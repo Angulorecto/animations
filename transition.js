4d29a5f96fe60ad69f769436b76cea6894049218
AnguloRecto <120491110+Angulorecto@users.noreply.github.com>
2024-02-13 15:19:04 -0500

    Update transition.js

--- transition.js
+++ transition.js
@@ -1,22 +1,22 @@
-const box1 = document.querySelector("#box1");
-const box2 = document.querySelector("#box2");
-const hammer = document.querySelector("#hammer");
+var box1 = document.getElementById("box1");
+var box2 = document.querySelector("#box2");
+var hammer = document.getElementById("hammer");
 
 box1.addEventListener("animationend", () => {
-  box1.style.bottom = "50%";
-  box2.style.top = "50%";
+  document.getElementById("box1").style.bottom = "50%";
+  document.getElementById("box2").style.top = "50%";
 });
 hammer.addEventListener("animationend", () => {
-  hammer.style.opacity = "100%";
-  hammer.style.left = "40%";
-  hammer.style.top = "10%";
-  hammer.style.width = "20%";
+  document.getElementById("hammer").style.opacity = "100%";
+  document.getElementById("hammer").style.left = "40%";
+  document.getElementById("hammer").style.top = "10%";
+  document.getElementById("hammer").style.width = "20%";
 });
 function start() {
-  box1.style.animation = "down 1s";
-  box2.style.animation = "up 1s";
-  box1.style.display = "block";
-  box2.style.display = "block";
-  hammer.style.animation = "fade 2s";
-  hammer.style.display = "block";
+  document.getElementById("box1").style.animation = "down 1s";
+  document.getElementById("box2").style.animation = "up 1s";
+  document.getElementById("box1").style.display = "block";
+  document.getElementById("box2").style.display = "block";
+  document.getElementById("hammer").style.animation = "fade 2s";
+  document.getElementById("hammer").style.display = "block";
 }
