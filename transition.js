945f286cb6f025926d2028b123eba21fb0c20452
AnguloRecto <120491110+Angulorecto@users.noreply.github.com>
2024-02-13 15:34:12 -0500

    Update transition.js

--- transition.js
+++ transition.js
@@ -1,22 +1,8 @@
-var box1 = document.getElementById("box1");
-var box2 = document.getElementById("box2");
-var hammer = document.getElementById("hammer");
-
-box1.addEventListener("animationend", () => {
-  box1.style.bottom = "50%";
-  box2.style.top = "50%";
-});
-hammer.addEventListener("animationend", () => {
-  hammer.style.opacity = "100%";
-  hammer.style.left = "40%";
-  hammer.style.top = "10%";
-  hammer.style.width = "20%";
-});
 function start() {
-  box1.style.animation = "down 1s forwards";
-  box2.style.animation = "up 1s forwards";
-  box1.style.display = "block";
-  box2.style.display = "block";
-  hammer.style.animation = "fade 2s forwards";
-  hammer.style.display = "block";
+  document.getElementById("box1").style.animation = "down 1s forwards";
+  document.getElementById("box2").style.animation = "up 1s forwards";
+  document.getElementById("box1").style.display = "block";
+  document.getElementById("box2").style.display = "block";
+  document.getElementById("hammer").style.animation = "fade 2s forwards";
+  document.getElementById("hammer").style.display = "block";
 }
