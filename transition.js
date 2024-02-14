4466f347c44eb463339946169a1d8a8d6abe951d
AnguloRecto <120491110+Angulorecto@users.noreply.github.com>
2024-02-14 12:39:01 -0500

    Update transition.js

--- transition.js
+++ transition.js
@@ -1,3 +1,9 @@
+function text() {
+  document.getElementById("title").style.animation = "textIn 1s forwards";
+  document.getElementById("title").style.display = "block";
+  document.getElementById("reason").style.animation = "textIn 1s forwards";
+  document.getElementById("reason").style.display = "block";
+}
 function start() {
   document.getElementById("box1").style.animation = "down 1s forwards";
   document.getElementById("box2").style.animation = "up 1s forwards";
@@ -5,8 +11,5 @@ function start() {
   document.getElementById("box2").style.display = "block";
   document.getElementById("hammer").style.animation = "fade 3s forwards";
   document.getElementById("hammer").style.display = "block";
-  document.getElementById("title").style.animation = "textIn 1s forwards";
-  document.getElementById("title").style.display = "block";
-  document.getElementById("reason").style.animation = "textIn 1s forwards";
-  document.getElementById("reason").style.display = "block";
+  setTimeout(text, 2000);
 }
