e8f05af8c02c1a6897df18b0e581d0e4815dbcb6
AnguloRecto <120491110+Angulorecto@users.noreply.github.com>
2024-02-15 11:12:59 -0500

    Update transition.js

--- transition.js
+++ transition.js
@@ -13,3 +13,12 @@ function start() {
   document.getElementById("hammer").style.display = "block";
   setTimeout(text, 3000);
 }
+function warn() {
+  document.getElementById("box1").style.animation = "down 1s forwards";
+  document.getElementById("box2").style.animation = "up 1s forwards";
+  document.getElementById("box1").style.display = "block";
+  document.getElementById("box2").style.display = "block";
+  document.getElementById("triangle").style.animation = "warn 3s forwards";
+  document.getElementById("triangle").style.display = "block";
+  setTimeout(text, 3000);
+}
