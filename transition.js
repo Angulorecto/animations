c34407fef715d80c1c08645bf4831d193801547c
AnguloRecto <120491110+Angulorecto@users.noreply.github.com>
2024-02-16 08:15:42 -0500

    Update transition.js

--- transition.js
+++ transition.js
@@ -13,6 +13,17 @@ function start() {
   document.getElementById("hammer").style.display = "block";
   setTimeout(text, 3000);
 }
+function banOut() {
+  document.getElementById("box1").style.animation = "fadeOut 3s forwards";
+  document.getElementById("box2").style.animation = "fadeOut 3s forwards";
+  document.getElementById("hammer").style.animation = "fadeOut 3s forwards";
+}
+function warnOut() {
+  document.getElementById("title").style.animation = "fadeOut 3s forwards";
+  document.getElementById("reason").style.animation = "fadeOut 3s forwards";
+  document.getElementById("triangle").style.animation = "fadeOut 3s forwards";
+  document.getElementById("glass").style.animation = "fadeOut 3s forwards";
+}
 function warn() {
   document.getElementById("title").style.animation = "warnText 3s forwards";
   document.getElementById("title").style.display = "block";
