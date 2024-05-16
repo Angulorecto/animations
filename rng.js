0e82bf35512e73491ac6262ffb22a5ac5ac1ef17
AnguloRecto <120491110+Angulorecto@users.noreply.github.com>
2024-05-16 11:51:24 -0400

    Create rng.js

---
+++ rng.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', function() {
+  document.getElementById("vignette").style.animation = "vignette 10s ease";
+  document.getElementById("star").style.animation = "star 10s ease";
+})
