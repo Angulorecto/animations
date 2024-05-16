138625100a1ae89b26b85b277bae1cc161742814
AnguloRecto <120491110+Angulorecto@users.noreply.github.com>
2024-05-16 14:50:25 -0400

    Update rng.js

--- rng.js
+++ rng.js
@@ -1,6 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
   document.getElementById("vignette").style.display = "block";
   document.getElementById("vignette").style.animation = "vignette 10s ease";
-  document.getElementById("vignette").style.display = "none";
   document.getElementById("star").style.animation = "star 10s ease";
 })
