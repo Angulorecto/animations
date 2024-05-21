6f04fd0cc992e03dfba672098e8149b715a822ca
AnguloRecto <120491110+Angulorecto@users.noreply.github.com>
2024-05-21 14:58:56 -0400

    Update rng.js

--- rng.js
+++ rng.js
@@ -8,5 +8,5 @@ function ani() {
 document.addEventListener('DOMContentLoaded', function() {
   document.getElementById("vignette").style.display = "block";
   document.getElementById("vignette").style.animation = "vignette 10s ease";
-  document.getElementById("star").style.animation = "10s cubic-bezier(1, 0, 0.68, 1.01) 0s 1 normal none running growStar";
+  document.getElementById("star").style.animation = "10s cubic-bezier(1, 0, 0.68, 1.01) 0s 1 normal none running centerAnimation";
 })
